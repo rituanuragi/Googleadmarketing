b9b98ffa16f46d2f845fe1dc576728a7a20efbfe
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-07-29 18:56:56 +0530

    Update server.js

--- contact_backend/server.js
+++ contact_backend/server.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
